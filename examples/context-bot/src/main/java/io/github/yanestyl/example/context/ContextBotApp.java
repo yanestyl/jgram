a1a9795465f0a7f6c02b790f96101f650e290f16
Yaroslav Starodub <yaroslavl.ja@yandex.ru>
2026-04-10 13:04:32 +0300

    Добавлен бот, демонстрирующий работу с контекстом

--- examples/context-bot/src/main/java/io/github/yanestyl/example/context/ContextBotApp.java
+++ examples/context-bot/src/main/java/io/github/yanestyl/example/context/ContextBotApp.java
@@ -1,12 +1,12 @@
-package io.github.yanestyl.example;
+package io.github.yanestyl.example.context;
 
 import io.github.yanestyl.jgram.annotation.JGramApplication;
 import io.github.yanestyl.jgram.core.JGram;
 
 @JGramApplication
-public class EchoBotApp {
+public class ContextBotApp {
 
     public static void main(String[] args) {
-        JGram.run(EchoBotApp.class, args);
+        JGram.run(ContextBotApp.class, args);
     }
 }
